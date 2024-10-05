d7eed411c91ea8e69a86048d60060b76f17276bf
watanabe-1 <96782802+watanabe-1@users.noreply.github.com>
2024-10-05 21:36:19 +0900

    prismaのホットリロード対策のために、ここでのみvarの使用を許可

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 declare global {
   // この宣言により、PrismaClientのインスタンスがグローバルに再利用されます（開発環境でのホットリロード対策）
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
