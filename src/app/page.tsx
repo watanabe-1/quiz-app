5a384c39dab2c45d98ab7d003db9d922fc571232
watanabe-1 <96782802+watanabe-1@users.noreply.github.com>
2024-10-12 17:48:38 +0900

    homeのtitle表示などを変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,13 @@ import Link from "next/link";
 import Header from "@/components/layout/Header";
 import { fetchGetAllQualifications } from "@/lib/api";
 import { createPath } from "@/lib/path";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "ホーム",
-};
 
 const HomePage = async () => {
   const qualifications = await fetchGetAllQualifications();
 
   return (
     <div>
-      <Header title="クイズアプリへようこそ" />
+      <Header title="過去問テストサイトへようこそ" />
       <main className="p-6">
         <h2 className="text-xl font-semibold mb-4">資格を選択してください</h2>
         <ul className="space-y-2">
