e02d292e87e65e0758f39eaa21d0a346681dec23
watanabe-1 <96782802+watanabe-1@users.noreply.github.com>
2024-11-04 15:52:44 +0900

    proxies/createQueryParamsProxy.tsは見つからない場合はundifind返す

--- src/lib/proxies/createQueryParamsProxy.ts
+++ src/lib/proxies/createQueryParamsProxy.ts
@@ -1,23 +1,27 @@
 /**
  * Creates a proxy for URLSearchParams to enable dynamic property access.
- * The proxy allows accessing URL query parameters as properties of an object.
+ * This proxy allows you to access URL query parameters as if they were properties of an object.
  *
  * @typeParam T - A generic type representing an object where keys are strings, and values are optional strings.
- * @param {URLSearchParams} searchParams - The URLSearchParams instance used to access query parameters.
- * @returns {T} A proxy object that allows dynamic access to query parameters by property names.
+ * @param {URLSearchParams} searchParams - The URLSearchParams instance used to retrieve query parameters.
+ * @returns {T} A proxy object that provides dynamic access to query parameters by property names.
  *
  * @example
  * const searchParams = new URLSearchParams("param1=value1&param2=value2");
- * const params = createQueryParamsProxy<{ param1: string | undefined, param2: string | undefined }>(searchParams);
+ * const params = createQueryParamsProxy<{ param1?: string, param2?: string }>(searchParams);
  * console.log(params.param1); // Outputs: "value1"
+ * console.log(params.param3); // Outputs: undefined
  */
-export const createQueryParamsProxy = <T extends Record<string, string>>(
+export const createQueryParamsProxy = <
+  T extends Record<string, string | undefined>,
+>(
   searchParams: URLSearchParams,
 ): T => {
   return new Proxy({} as T, {
     get: (_, prop: string) => {
-      // Returns the value for the given prop, or undefined if not present
-      return decodeURIComponent(searchParams.get(prop) ?? "");
+      // Returns the decoded value for the given prop, or undefined if not present
+      const value = searchParams.get(prop);
+      return value ? decodeURIComponent(value) : undefined;
     },
   });
 };
