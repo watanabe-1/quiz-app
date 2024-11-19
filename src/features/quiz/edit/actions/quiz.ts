8a70994bc2ff579218a8882d037636d75146ce43
watanabe-1 <96782802+watanabe-1@users.noreply.github.com>
2024-11-19 20:30:33 +0900

    update: リダイレクト処理を追加し、更新後の管理画面へ遷移を実装

--- src/features/quiz/edit/actions/quiz.ts
+++ src/features/quiz/edit/actions/quiz.ts
@@ -2,8 +2,10 @@
 
 import { type SubmissionResult } from "@conform-to/react";
 import { parseWithZod } from "@conform-to/zod";
+import { redirect } from "next/navigation";
 import { questionDataSchema } from "@/features/quiz/edit/lib/schema";
 import { revalidateTagByUpdateQuestion } from "@/lib/api";
+import { path_admin_qualification_grade_year } from "@/lib/path";
 import { existsQuestion, saveQuestion } from "@/services/quizService";
 
 type FormState =
@@ -61,6 +63,11 @@ export const updateQuiz = async (
 
   revalidateTagByUpdateQuestion();
 
+  redirect(
+    path_admin_qualification_grade_year(qualification, grade, year).$url().path,
+  );
+
+  // ここに到達しない想定だが一応記載
   return {
     status: "success",
     message: "更新に成功しました",
