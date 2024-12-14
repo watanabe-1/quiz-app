16a252b544ec7364b9e461b04facdfc90a53984d
watanabe-1 <96782802+watanabe-1@users.noreply.github.com>
2024-12-14 21:06:41 +0900

    feat: Add middleware matcher configuration to exclude static and image paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,3 +3,17 @@ import { withLogin } from "@/middlewares/withLogin";
 
 // ミドルウェアを連結
 export default chainMiddlewares([withLogin]);
+
+export const config = {
+  matcher: [
+    /**
+     * 以下のパスを除外しています：
+     * - `_next/static`: Next.jsのビルド静的ファイル（例: CSS, JS）
+     * - `_next/image`: Next.jsの画像最適化エンドポイント
+     * - `favicon.ico`: サイトのファビコン
+     * - 画像拡張子を持つ静的ファイル（例: `.svg`, `.png`, `.jpg`など）
+     *
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
